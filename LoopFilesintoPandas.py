ff762b324ca1df997a7e4cf6882a908ee36f1930
Alex <68165007+Alexdowd2@users.noreply.github.com>
2022-03-17 12:37:53 -0400

    Add files via upload

---
+++ LoopFilesintoPandas.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+from pathlib import Path
+import os.path, time
+import glob
+
+
+# In[ ]:
+
+
+#establish empty list
+init_df = []
+#for loop to iterate through files in folder, read into pandas, then append to list
+for i in glob.glob('filePath\\*.xlsx'):
+    this_df = pd.read_excel(i, sheet_name = 'mySheet')
+    init_df.append(this_df)
+#create a new dataframe from the init_df list
+master_df = pd.concat(init_df)
+#write the file to excel
+master_df.to_excel('filePath', index = False)
+
+
+
